f624f6067389ace471f5324cdec4d25522326af2
Thomas PETIT-JEAN <thomas.petitjean@aphp.fr>
2022-12-02 18:22:46 +0100

    Bump to v0.1.1

--- changelog.md
+++ changelog.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v0.1.1 (2022-12-02)
 
 ### Added
 - Various project metadata
--- eds_scikit/__init__.py
+++ eds_scikit/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for eds_scikit."""
 
 __author__ = """eds_scikit"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 import importlib
 import os
