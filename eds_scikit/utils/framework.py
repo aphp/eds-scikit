e82636ee32beba7613c784ec844ad0864b701b9c
Vincent M <maladiere.vincent@yahoo.fr>
2022-12-08 19:37:18 +0100

    add framework object

--- eds_scikit/utils/framework.py
+++ eds_scikit/utils/framework.py
@@ -1,80 +1,92 @@
+from collections import Counter
+from functools import partial
 from types import ModuleType
-from typing import Dict, Optional
+from typing import Optional
 
-import pandas as _pandas
-from databricks import koalas as _koalas
+import pandas as pd
+from databricks import koalas as ks
 
-from eds_scikit.utils.typing import DataFrame, DataObject
+from eds_scikit.utils.typing import DataObject
 
-VALID_FRAMEWORKS = {
-    "pandas": _pandas,
-    "koalas": _koalas,
-}
+from .custom_implem import CustomImplem
 
-
-def get_framework(obj: DataObject) -> Optional[ModuleType]:
-    for _, framework in VALID_FRAMEWORKS.items():
-        if obj.__class__.__module__.startswith(framework.__name__):
-            return framework
-    # raise ValueError(f"Object from unknown framework: {obj}")
-    return None
+VALID_FRAMEWORKS = [pd, ks]
 
 
-def is_pandas(obj: DataObject) -> bool:
-    return get_framework(obj) == _pandas
+class BaseFramework:
+    """Dispatcher between pandas, koalas and custom method."""
 
+    def get_framework(self, obj: DataObject) -> Optional[ModuleType]:
+        for framework in VALID_FRAMEWORKS:
+            if obj.__class__.__module__.startswith(framework.__name__):
+                return framework
+        return None
 
-def is_koalas(obj: DataObject) -> bool:
-    return get_framework(obj) == _koalas
+    def is_pandas(self, obj: DataObject) -> bool:
+        return self.get_framework(obj) is pd
 
+    def is_koalas(self, obj: DataObject) -> bool:
+        return self.get_framework(obj) is ks
 
-def to(framework: str, obj: DataObject) -> DataObject:
-    possible_values = set(VALID_FRAMEWORKS.keys()).union(VALID_FRAMEWORKS.values())
-    assert framework in possible_values
-    if framework == "koalas" or framework is _koalas:
-        return koalas(obj)
-    elif framework == "pandas" or framework is _pandas:
-        return pandas(obj)
-    else:
-        raise ValueError(f"Unknown framework: {framework}")
+    def to_pandas(self, obj: DataObject) -> DataObject:
+        if self.get_framework(obj) is pd:
+            return obj
+        try:
+            return obj.to_pandas()
+        except AttributeError:
+            raise ValueError("Could not convert object to pandas.")
 
+    def to_koalas(self, obj: DataObject) -> DataObject:
+        if self.get_framework(obj) is ks:
+            return obj
+        try:
+            return obj.to_koalas()
+        except AttributeError:
+            pass
+        # Will raise ValueError if impossible
+        return ks.from_pandas(obj)
 
-def dict_to(framework: str, d: Dict[str, DataObject]) -> Dict[str, DataObject]:
-    d_converted = dict()
-    for k, v in d.items():
-        if is_pandas(v) or is_koalas(v):
-            d_converted[k] = to(framework, v)
+    def __getattr__(self, method):
+        return partial(self.get_params, method)
+
+    def get_params(self, method, *args, **kwargs):
+        fk = self._get_framework_from_params(*args, **kwargs)
+        if method in dir(fk):
+            # Use the native method
+            return getattr(fk, method)(*args, **kwargs)
+        elif method in dir(CustomImplem):
+            # Look for our implementation
+            use_pd = self.is_pandas(fk)
+            return getattr(CustomImplem, method)(*args, use_pd=use_pd, **kwargs)
         else:
-            d_converted[k] = v
-    return d_converted
+            raise NotImplementedError(
+                f"Method {method} doesn't belong to {fk.__name__} "
+                f"and is not implemented in eds_scikit yet."
+            )
+
+    def _get_framework_from_params(self, *args, **kwargs):
+        counter = Counter()
+        all_args = [*args, *kwargs.values()]
+        self._count_framework_from_args(all_args, counter)
+        if bool(counter["ks"]) and bool(counter["pd"]):  # "^" is the XOR operator
+            raise ValueError(
+                "Inputs have mixed types of Koalas and Pandas dataframes,"
+                "which is not supported.\n"
+                "Please convert your dataframes using fw.to_pandas(df) or fw.to_koalas(df)"
+            )
+        elif counter["pd"]:
+            return pd
+        elif counter["ks"]:
+            return ks
+        else:
+            return None
+
+    def _count_framework_from_args(self, all_args, counter):
+        for arg in all_args:
+            if isinstance(arg, (list, set, tuple)):
+                self._count_framework_from_args(arg, counter)
+            counter["pd"] += self.is_pandas(arg)
+            counter["ks"] += self.is_koalas(arg)
 
 
-def pandas(obj: DataObject) -> DataObject:
-    if get_framework(obj) is _pandas:
-        return obj
-    try:
-        return obj.to_pandas()
-    except AttributeError:
-        pass
-    raise ValueError("Could not convert object to pandas.")
-
-
-def koalas(obj: DataObject) -> DataObject:
-    if get_framework(obj) is _koalas:
-        return obj
-    try:
-        return obj.to_koalas()
-    except AttributeError:
-        pass
-
-    # will raise ValueError if impossible
-    return _koalas.from_pandas(obj)
-
-
-def add_unique_id(obj: DataFrame, col_name: str = "id") -> DataFrame:
-    fw = get_framework(obj)
-    if fw == _pandas:
-        obj[col_name] = range(len(obj))
-        return obj
-    else:
-        return obj.koalas.attach_id_column(id_type="distributed", column=col_name)
+fw = BaseFramework()
