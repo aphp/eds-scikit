4f20bc5f513f8d58f335ce2e896e27ef0b64d04d
Vincent M <maladiere.vincent@yahoo.fr>
2023-04-06 12:11:52 +0200

    improve outliers filtering

--- eds_scikit/plot/data_quality.py
+++ eds_scikit/plot/data_quality.py
@@ -29,18 +29,21 @@ def plot_age_pyramid(
         - `person_id`, dtype : any
         - `gender_source_value`, dtype : str, {'m', 'f'}
 
-    datetime_ref : Union[datetime, str],
+    datetime_ref : Union[datetime, str], default None
         The reference date to compute population age from.
         If a string, it searches for a column with the same name in the person table: each patient has his own datetime reference.
         If a datetime, the reference datetime is the same for all patients.
         If set to None, datetime.today() will be used instead.
 
-    savefig : bool,
+    filename : str, default None
+        The path to save figure at.
+
+    savefig : bool, default False
         If set to True, filename must be set.
         The plot will be saved at the specified filename.
 
-    filename : Optional[str],
-        The path to save figure at.
+    return_array : bool, default False
+        If set to True, return chart and its pd.Dataframe representation.
 
     Returns
     -------
@@ -90,8 +93,20 @@ def plot_age_pyramid(
     person_["age"] = (datetime_ref - person_["birth_datetime"]).dt.total_seconds()
     person_["age"] /= 365 * 24 * 3600
 
-    mask = person_["age"] > 90
-    person_.loc[mask, "age"] = 99
+    # Remove outliers
+    mask_age_inliners = (person_["age"] > 0) & (person_["age"] < 125)
+    n_outliers = (~mask_age_inliners).sum()
+    if n_outliers > 0:
+        perc_outliers = 100 * n_outliers / person_.shape[0]
+        print(
+            f"{n_outliers} ({perc_outliers:.1f}%) individuals' "
+            "age is out of the (0, 125) interval, we skip them."
+        )
+    person_ = person_.loc[mask_age_inliners]
+
+    # Aggregate rare age categories
+    mask_rare_age_agg = person_["age"] > 90
+    person_.loc[mask_rare_age_agg, "age"] = 99
 
     bins = np.arange(0, 100, 10)
     labels = [f"{left}-{right}" for left, right in zip(bins[:-1], bins[1:])]
