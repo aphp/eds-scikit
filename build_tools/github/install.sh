a73c4be26f6b23bc471455e9ab22e53e37bfb1f6
Thomas PETIT-JEAN <thomas.petitjean@aphp.fr>
2023-04-20 11:54:33 +0200

    chore: cap pip to 23

--- build_tools/github/install.sh
+++ build_tools/github/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
+pip install -U "pip<23"
 pip install --progress-bar off --upgrade ".[dev, doc]"
--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
 
+pip install -u "pip<23"
 python -m pytest --pyargs tests -m "" --cov=eds_scikit
