9b4e5e3772a2196f094a70947dce8a91fa0057e7
VITTOZ Simon <simon.vittoz-ext@aphp.fr>
2024-02-02 11:02:10 +0100

    bioclean

--- eds_scikit/biology/cleaning/main.py
+++ eds_scikit/biology/cleaning/main.py
@@ -4,7 +4,7 @@ from typing import List, Union
 from loguru import logger
 
 from eds_scikit.biology.cleaning.cohort import select_cohort
-from eds_scikit.biology.utils.process_concepts import ConceptsSet
+from eds_scikit.biology.utils.process_concepts import ConceptsSet, fetch_all_concepts_set
 from eds_scikit.biology.utils.prepare_measurement import prepare_measurement_table
 from eds_scikit.biology.viz.wrapper import plot_biology_summary
 from eds_scikit.io import settings
@@ -30,20 +30,14 @@ def bioclean(
         Instantiated [``HiveData``][eds_scikit.io.hive.HiveData], [``PostgresData``][eds_scikit.io.postgres.PostgresData] or [``PandasData``][eds_scikit.io.files.PandasData]
     concepts_sets : List[ConceptsSet], optional
         List of concepts-sets to select
-    config_name : str, optional
-        Name of the dataset used to [transform][eds_scikit.biology.cleaning.transform.transform_measurement] the data.
     start_date : datetime, optional
         **EXAMPLE**: `"2019-05-01"`
     end_date : datetime, optional
         **EXAMPLE**: `"2022-05-01"`
+    convert_units : bool, optional
+        If True, convert units based on ConceptsSets Units object. Eager execution., by default False
     studied_cohort : Union[DataFrame, np.iterable, set], optional
         List of patient_ids to select
-    clip : bool, optional
-        If `True` extreme values are set equal to the thresholds
-    standard_terminologies : List[str], optional
-        **EXAMPLE**: `["LOINC", "AnaBio"]`
-    standard_concept_regex : dict, optional
-        **EXAMPLE**: `{"LOINC": "[0-9]{2,5}[-][0-9]","AnaBio": "[A-Z][0-9]{4}"}`
 
     Returns
     -------
@@ -68,5 +62,5 @@ def bioclean(
 
     # Plot values
     value_column = "value_as_number_normalized" if convert_units else "value_as_number"
-    plot_biology_summary_measurement(measurements, value_column)
+    plot_biology_summary(measurements, value_column)
 
