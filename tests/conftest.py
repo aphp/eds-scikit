9831c0b4d493c03c77a74d9291b45ba13096273e
svittoz <simon.vittoz-ext@aphp.fr>
2024-02-08 14:35:14 +0000

    solve confest issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from _pytest.logging import caplog as _caplog  # noqa F401
 from databricks import koalas as ks
 from loguru import logger
 
-from eds_scikit import improve_performances, load_koalas, set_env_variables
+from eds_scikit.io.improve_performance import load_koalas, improve_performances, set_env_variables
 
 from . import test_registry  # noqa: F401 --> To register functions
 
