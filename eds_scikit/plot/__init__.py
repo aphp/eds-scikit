874010bffc9f9b61909028de0f1a3ea3fe3fd247
VITTOZ Simon <simon.vittoz-ext@aphp.fr>
2024-04-03 17:48:21 +0200

    add omop teva module

---
+++ eds_scikit/plot/__init__.py
@@ -0,0 +1,3 @@
+from eds_scikit.plot.table_viz import map_column, reduce_table, visualize_table
+from eds_scikit.plot.omop_teva import generate_omop_teva
+from eds_scikit.io.omop_teva_default_config import default_omop_teva_config
