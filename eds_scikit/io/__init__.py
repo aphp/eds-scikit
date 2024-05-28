09b7f50f70f8b0634a5a686af46217b13312dcb9
svittoz <137794505+svittoz@users.noreply.github.com>
2024-05-28 13:57:58 +0200

    pyarrow fix (#65)
    pyarrow path fix for executor

--- eds_scikit/io/__init__.py
+++ eds_scikit/io/__init__.py
@@ -2,7 +2,13 @@ from .base import BaseData
 from .files import PandasData
 from .hive import HiveData
 from .postgres import PostgresData
-from .improve_performance import improve_performances, koalas_options, load_koalas
+from .improve_performance import (
+    improve_performances,
+    koalas_options,
+    load_koalas,
+    pyarrow_fix,
+)
+
 
 __all__ = [
     "BaseData",
